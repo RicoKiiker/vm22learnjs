9b314aa14092d8dfbfda35f24f57dba155a8a31d
Rico Kiiker <Rico.kiiker@tptlive.ee>
2023-09-27 14:08:29 +0300

    add js number string

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('hello world')
+
+let num = 1;
+num = 3;
+num = 3 * 3 + 7 / 2 -15;
+num = num + 3;
+num += 3;
+num -= 2;
+num *= 3;
+num /= 2;
+num ++;
+num --;
+num = num **2;
+num **=2;
+num = 13;
+num = num % 7;
+num %= 2;
+//var text = 'Hello'; Dont use var
+console.log(num);
+let string = 'Hello';
+string = "Hello don't";
+string = `hello
+im multi line
+string`;
+string = `Hello number is: ${num}`;
+string = 'Hello number is: ' + num;
