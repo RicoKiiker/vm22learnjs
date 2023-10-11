35135ca97a3279498a4aebf3e3a6b9626fab6c7d
Rico Kiiker <Rico.kiiker@tptlive.ee>
2023-10-11 13:42:39 +0300

    For

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-let array = [1, 2, true, 1.2, 'hello'];
-console.log(array);
-console.log(array[4]);
+for(let i=0; i<10; i++){
+console.log(i);
+}
+let i = 1;
+while(i<1_000_000){
+    console.log(i);
+i*=2;
+}
 
-array[3] = 96;
-console.log(array);
-array.push('new value');
-console.log(array);
-array = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-];
-console.log(array);
-console.log(array[1][2]);
-array = [[[[[1]]]]]
-console.log(array);
-console.log(array[0][0][0][0][0]);
+i = 1;
+while(i>10){
+    console.log('asjklds');
+}
+
+i = 1;
+do{
+    console.log('asjklds');
+} while(i>10);
+
+let fruits=['banana', 'cherry', 'apple', 'pear'];
+
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
 
 let object = {
-name: 'Rico',
-age: 17,
-cats: ['Rei', 'Miu']
-};
-console.log(object);
-console.log(object.name);
+    name: 'Rõve Pede',
+    age: 3,
+    pronouns: 'Attack/Helicopter'
+}
+
+for(const property in object){
+    console.log(property);
+    console.log(object[property]);
+}
