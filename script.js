a18f5fe16bd6d2d5e6880fb18ab7e7b72aae363e
Rico Kiiker <Rico.kiiker@tptlive.ee>
2023-10-11 14:08:56 +0300

    while

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-for(let i=0; i<10; i++){
-console.log(i);
-}
-let i = 1;
-while(i<1_000_000){
+let i = 3;
+
+while(i<1_000){
     console.log(i);
-i*=2;
+    i*=5;
 }
 
-i = 1;
-while(i>10){
-    console.log('asjklds');
-}
-
-i = 1;
-do{
-    console.log('asjklds');
-} while(i>10);
-
-let fruits=['banana', 'cherry', 'apple', 'pear'];
-
-for(let i=0; i<fruits.length; i++){
-    console.log(fruits[i]);
-}
-
-for(const fruit of fruits){
-    console.log(fruit);
-}
-
-let object = {
-    name: 'Rõve Pede',
-    age: 3,
-    pronouns: 'Attack/Helicopter'
-}
-
-for(const property in object){
-    console.log(property);
-    console.log(object[property]);
-}
