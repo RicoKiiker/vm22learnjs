04dc08ddc3f73f4251209b96b10f833c1dd7309a
Rico Kiiker <Rico.kiiker@tptlive.ee>
2023-10-18 13:06:24 +0300

    If, case, break

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-let i = 3;
-
-while(i<1_000){
-    console.log(i);
-    i*=5;
+let num = 10;
+if(num> 10){
+    console.log('suurem');
+} else if(num === 10){
+    console.log('võrdne');
+} else {
+    console.log('väiksem');
 }
 
+let day = new Date('2023-10-20').getDay();
+console.log(day);
+if(day === 0){
+    console.log('Pühapäev');
+} else if(day === 1){
+    console.log('Esmaspäev');
+}
+
+switch(day){
+    case 0:
+        console.log('Pühapäev');
+        break;
+        case 1:
+            console.log('Esmaspäev');
+            break;
+            case 2:
+                console.log('Teisipäev');
+            break;
+            case 3:
+                console.log('Its Wednesday My Dudes!');
+            break;
+            case 4:
+                console.log('Neljapäev');
+            break;
+            case 5:
+                console.log('Its friday, Its friday, Its friday...');
+                case 6:
+                    console.log('PartyDay');
+            break;
+            default:
+                console.log('weirdday');
+}
