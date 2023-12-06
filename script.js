809441b239e19a22321c467bbbef428e6433a707
Rico Kiiker <Rico.kiiker@tptlive.ee>
2023-12-06 13:58:57 +0200

    add form example

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ nunjucks.configure('views', {
     express: app
 });
 
+app.use(express.urlencoded());
 
 app.get('/', function (req, res) {
-    let name = "Rico Kiiker"
-    res.render('index.njk', {name});
+    let name = req.query.name;
+    let age = req.query.age;
+    res.render('index.njk', {name, age});
+});
+
+app.post('/answer', function (req, res) {
+    let name = req.body.name;
+    let age = req.body.age;
+    res.render('answer.njk', {name, age});
 });
 
 app.get('/about', function (req, res) {
